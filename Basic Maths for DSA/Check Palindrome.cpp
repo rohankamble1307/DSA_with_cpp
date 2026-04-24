48a95f6dbb80ef1f9204c49944c0c6bb64e9719f
Rohan Kamble <151061896+rohankamble1307@users.noreply.github.com>
2026-04-24 18:35:39 +0530

    Add comments for palindrome check example

---
+++ Basic Maths for DSA/Check Palindrome.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+
+    int temp = n;
+    int rev = 0;
+
+    while(n > 0){
+        int last = n % 10;
+        rev = rev * 10 + last;
+        n = n / 10;
+    }
+
+    if(rev == temp) cout << "Palindrome";
+    else cout << "Not Palindrome";
+}
+
+/*
+Input: 121
+Output: Palindrome
+
+Input: 123
+Output: Not Palindrome
+
+*/
