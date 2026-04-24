94b1df2a69f419efc4e7b9b4969271b8e1ded304
Rohan Kamble <151061896+rohankamble1307@users.noreply.github.com>
2026-04-24 18:25:40 +0530

    Add example of unordered_set usage
    Implement basic usage of unordered_set in C++.

---
+++ STL/Unordered Set.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+    unordered_set<int> st;
+
+    st.insert(1); // {1}
+    st.insert(2); // {1,2}
+    st.insert(3); // {1,2,3}
+
+    // unique
+    // no sorted order
+    // O(1) average
+    // worst case O(n) (collision)
+
+}
