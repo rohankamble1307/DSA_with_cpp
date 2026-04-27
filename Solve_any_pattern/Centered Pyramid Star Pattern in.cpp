5503cfb9f4c668b2c0c95a57779ef0e524f5580f
Rohan Kamble <151061896+rohankamble1307@users.noreply.github.com>
2026-04-27 11:34:35 +0530

    Fix order of star and space printing in pyramid pattern
    Fixed the order of printing stars and spaces in the centered pyramid pattern.

--- Solve_any_pattern/Centered Pyramid Star Pattern in.cpp
+++ Solve_any_pattern/Centered Pyramid Star Pattern in.cpp
@@ -10,7 +10,7 @@ void print7(int n) {
         for (int j = 0; j<2*i+1; j++) {
             cout << "*"; 
         }
-        // Print stars
+        // Print spaces
         for (int j = 0; j<n-i-1; j++){
             cout << " ";
         }
