a9ca040c1922c55d0bdc4b37cad340a3d3163eb4
Rohan Kamble <151061896+rohankamble1307@users.noreply.github.com>
2026-04-27 11:05:37 +0530

    Fix formatting of stack push comments in Stack.cpp

--- STL/Stack.cpp
+++ STL/Stack.cpp
@@ -5,10 +5,10 @@ int main() {
 
     stack<int> st;
 
-    st.push(1);    // {1}
-    st.push(2);    // {2,1}
-    st.push(3);    // {3,2,1}
-    st.emplace(5); // {5,3,2,1}
+    st.push(1);    //  {1}
+    st.push(2);    //  {2,1}
+    st.push(3);    //  {3,2,1}
+    st.emplace(5); //  {5,3,2,1}
 
     cout << st.top(); // 5
 
